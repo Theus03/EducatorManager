5cbb19b90786de7e45d8511c0dd043e00b6ef5f0
Matheus Leite <matheusssleite@outlook.com>
2025-03-26 11:31:44 -0300

    Ajustando componente header da page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <div className="flex justify-items-center">
-            <Image
-                className=""
-                src="/logo.svg"
-                alt="Educator Manager logo"
-                width={180}
-                height={38}
-                priority
-                />
-            <nav >
-
+        <div className="w-screen flex justify-items-center items-center">
+            <nav className="flex items-center gap-16" >
+                <Image
+                    className=""
+                    src="/logo.svg"
+                    alt="Educator Manager logo"
+                    width={180}
+                    height={38}
+                    priority
+                    />
+                <ul className="flex gap-8">
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
             </nav>
         </div>
     )
